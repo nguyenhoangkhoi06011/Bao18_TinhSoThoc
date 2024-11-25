5b6a90e2251d9f8722edf7d0165922e4dd29427a
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-25 10:23:45 +0700

    iomanip

--- Bao18_TinhSoThoc.cpp
+++ Bao18_TinhSoThoc.cpp
@@ -1,5 +1,6 @@
 #include<cmath>
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main()
 {
@@ -7,13 +8,22 @@ int main()
 	int SoO;
 	double tongthoc = 0;
 	cout << "nhap so o:"; cin >> SoO;
-
+	cout.imbue(locale("en_US.UTF-8")); //phan nghin
+	if (SoO > 64) {
+		cout << "tren ban chi co 64o,vui long nhap so<=64\n";
+		system("pause");
+		return 0;
+		do 
+		{
+			cout << "nhap so o:"; cin >> SoO;
+		} while (SoO >64 || SoO<0);
+	}
 	for (int i = 0; i <SoO; i++)
 	{
-		cout << "o thu" << i + 1 << "co" << pow(2, i) << "thoc\n";
+		cout << "o thu" << i + 1 << "co\t\t"<<fixed<<setprecision(0) << pow(2, i) << "hat thoc\n";
 		tongthoc = tongthoc + pow(2, i);
 	}
-	cout << "tong so thoc la:" << tongthoc;
+	cout << "tong so thoc la:\t" << tongthoc << "hat thoc\n";
 	return 0;
 }
 
